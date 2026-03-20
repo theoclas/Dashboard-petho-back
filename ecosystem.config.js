2038ecfe589f162e170e58ea286fe37bb887d255
Fer_suarez01 <41597684+theoclas@users.noreply.github.com>
2026-03-20 00:29:47 -0500

    cambios +30m para procesos

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: false,
-        max_memory_restart: '512M',
+        max_memory_restart: '1G',
         env_file: '.env',
         env: {
           NODE_ENV: 'production',

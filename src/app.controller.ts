278c0151fcf58b08d5cb5ecd0227de0e3f65f963
Fer_suarez01 <41597684+theoclas@users.noreply.github.com>
2026-03-19 21:54:25 -0500

    feat: add ping and ping-auth endpoints for testing connection and token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('ping')
+  ping() {
+    return { status: 'Backend de Petho esta ONLINE!', timestamp: new Date().toISOString() };
+  }
+
+  @Get('ping-auth')
+  @UseGuards(JwtAuthGuard)
+  pingAuth(@Req() req) {
+    return { status: 'El token funciona perfectamente!', user: req.user };
   }
 }
